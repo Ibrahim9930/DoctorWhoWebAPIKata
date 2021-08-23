8bd6e1a3a444b33b82dbb1987b46ecbb08e8aa1a
Ibrahim Masri <msri.ibraheem@gmail.com>
2021-08-23 10:39:54 +0300

    Add mapper from Doctor to DoctorDto

---
+++ DoctorWho.Web/Profiles/DoctorsProfile.cs
@@ -0,0 +1,14 @@
+using AutoMapper;
+using DoctorWho.Db.Domain;
+using DoctorWho.Web.Models;
+
+namespace DoctorWho.Web.Profiles
+{
+    public class DoctorsProfile  : Profile
+    {
+        public DoctorsProfile()
+        {
+            CreateMap<Doctor, DoctorDto>();
+        }
+    }
+}
