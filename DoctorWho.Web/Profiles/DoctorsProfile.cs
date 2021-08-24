7025d3c288dcd359945103ec1c3f343f8eb55f25
Ibrahim Masri <msri.ibraheem@gmail.com>
2021-08-24 17:46:17 +0300

    Add mapper for Doctor PUT Upsert

--- DoctorWho.Web/Profiles/DoctorsProfile.cs
+++ DoctorWho.Web/Profiles/DoctorsProfile.cs
@@ -1,15 +1,18 @@
+using System;
 using AutoMapper;
 using DoctorWho.Db.Domain;
 using DoctorWho.Web.Models;
 
 namespace DoctorWho.Web.Profiles
 {
-    public class DoctorsProfile  : Profile
+    public class DoctorsProfile : Profile
     {
         public DoctorsProfile()
         {
             CreateMap<Doctor, DoctorDto>();
             CreateMap<DoctorForCreationWithPostDto, Doctor>();
+            CreateMap<DoctorForUpsertWithPut, Doctor>();
         }
+
     }
 }
