46ac1d55adc7505a828bf083fc075a5f297d3998
Ibrahim Masri <msri.ibraheem@gmail.com>
2021-08-28 21:30:17 +0300

    Add get specific resource tests for EpisodeController

--- DoctorWho.Tests/EpisodeControllerApiTests/GetTests.cs
+++ DoctorWho.Tests/EpisodeControllerApiTests/GetTests.cs
@@ -15,14 +15,26 @@ namespace DoctorWho.Tests.EpisodeControllerApiTests
         {
         }
         
-        [Fact]
-        public async Task GET_EpisodeController_StatusCode_Should_200StatusCode()
+        [Theory]
+        [InlineData("/api/episodes")]
+        [InlineData("/api/episodes/S01:E02")]
+        public async Task GET_EpisodeController_StatusCode_Should_200StatusCode(string uri)
         {
             var client = Fixture.CreateClient();
 
-            var response = await client.GetAsync("api/episodes");
+            var response = await client.GetAsync(uri);
 
             response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+        
+        [Fact]
+        public async Task GET_EpisodeController_ResourceDoesNotExist_StatusCode_Should_404StatusCode()
+        {
+            var client = Fixture.CreateClient();
+
+            var response = await client.GetAsync("/api/episodes/S20:E20");
+
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
         }
         
         [Fact]
@@ -34,6 +46,17 @@ namespace DoctorWho.Tests.EpisodeControllerApiTests
             var episodes = await ResponseParser.GetObjectFromResponse<ICollection<EpisodeDto>>(response);
             
             episodes.Should().HaveCount(10);
+        }
+        
+        [Fact]
+        public async Task GET_EpisodeController_Episode_Response_Should_HasNumberSeries1()
+        {
+            var client = Fixture.CreateClient();
+            
+            var response = await client.GetAsync("/api/episodes/S01:E02");
+            var episode = await ResponseParser.GetObjectFromResponse<EpisodeDto>(response);
+            
+            episode.SeriesNumber.Should().Be(1);
         }
     }
 }
